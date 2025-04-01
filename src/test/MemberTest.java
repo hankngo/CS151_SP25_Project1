c3f1e7cef7911f322a24e9cadd837bc0fa306bd3
Aetius Gular <142845190+aetiusgular@users.noreply.github.com>
2025-04-01 11:38:24 -0700

    Member JUNIT tests adjustments V2

--- src/test/MemberTest.java
+++ src/test/MemberTest.java
@@ -1,10 +1,16 @@
-package membership;
+/* 
+* This the extra credit JUNIT test class for the membership functionality 
+*/
+package test;
 
+import membership.Member;
+import membership.MemberManager;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MemberTest {
 
+    /* tests for invalid phone number length */
     @Test
     void testInvalidPhoneNumberLength() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> 
@@ -13,6 +19,7 @@ class MemberTest {
         assertEquals("Invalid phone number. Must be 10 digits.", exception.getMessage());
     }
 
+    /* tests for duplicate phone numbers */
     @Test
     void testDuplicatePhoneNumber() {
         MemberManager.signUp("Alice", "Smith", "1234567890", Member.Tier.SILVER);
@@ -22,6 +29,7 @@ class MemberTest {
         assertEquals("Invalid phone number. Phone number already in use.", exception.getMessage());
     }
 
+    /* tests for if the user puts no first name */
     @Test
     void testNoFirstName() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> 
@@ -30,6 +38,7 @@ class MemberTest {
         assertEquals("No first name provided.", exception.getMessage());
     }
 
+    /* tests for if the user puts no last name */
     @Test
     void testNoLastName() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> 
@@ -38,6 +47,7 @@ class MemberTest {
         assertEquals("No last name provided.", exception.getMessage());
     }
 
+    /* tests for if customer tries to use their points but their blance is insufficient */
     @Test
     void testInsufficientPointsException() {
         Member member = new Member("Jane", "Doe", "9876543210", Member.Tier.GOLD);
